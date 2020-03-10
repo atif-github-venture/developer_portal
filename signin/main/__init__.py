0d778fc6aecb7aa6251e7b9bbdb1cefb74e57bd7
aahmed <aahmed@lb.com>
2020-03-10 15:25:38 -0400

    APM inprogress

--- signin/main/__init__.py
+++ signin/main/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_bcrypt import Bcrypt
 from .config import config_by_name
+from elasticapm.contrib.flask import ElasticAPM
+from elasticapm.handlers.logging import LoggingHandler
 
 db = MongoEngine()
 flask_bcrypt = Bcrypt()
@@ -9,6 +11,7 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
+    apm = ElasticAPM(app, server_url='http://localhost:8200', service_name='auth-service', logging=True)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
---
+++ signin/readme.md
@@ -0,0 +1,5 @@
+# cd navtest
+#docker image rm -f signin
+#docker build -t signin .
+#docker run -p 8080:8080 -it signin
+#docker ps
