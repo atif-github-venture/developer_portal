f97c4d5d2dc74b98d486e6b8f3efff598f0eeab6
aahmed <aahmed@lb.com>
2020-03-18 16:37:44 -0400

    access rule implemented

--- devportal_flaskapi/main/model/accessrule.py
+++ devportal_flaskapi/main/model/accessrule.py
@@ -4,6 +4,6 @@ from .. import db
 class AccessRules(db.Document):
     # id = db.StringField(primary_key=True, autoincrement=True)
     projectname = db.StringField(unique=True, nullable=False)
-    groups = db.ListField(max_length=100)
-    users = db.ListField(max_length=100)
+    groups = db.ListField()
+    users = db.ListField()
     registered_on = db.DateTimeField(nullable=False)
--- devportal_flaskapi/main/model/group.py
+++ devportal_flaskapi/main/model/group.py
@@ -4,5 +4,5 @@ from .. import db
 class Groups(db.Document):
     # id = db.StringField(primary_key=True, autoincrement=True)
     groupname = db.StringField(unique=True, nullable=False)
-    users = db.ListField(max_length=100)
+    users = db.ListField()
     registered_on = db.DateTimeField(nullable=False)
