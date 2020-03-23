edeeb6e03ee05575a47511b341d53a9004ec77c0
aahmed <aahmed@lb.com>
2020-03-23 16:49:15 -0400

    Developer portal in making - password masking

--- devportal_django_ui/ui/forms.py
+++ devportal_django_ui/ui/forms.py
@@ -4,9 +4,9 @@ from django import forms
 class RegistrationForm(forms.Form):
     email = forms.EmailField(max_length=200)
     username = forms.CharField(max_length=200)
-    password = forms.CharField(max_length=200)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
 
 
 class LoginForm(forms.Form):
     email = forms.EmailField(max_length=200)
-    password = forms.CharField(max_length=200)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
