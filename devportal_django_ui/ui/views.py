1ae564b774861a9df14f061e0e05ccf3092494be
aahmed <aahmed@lb.com>
2020-03-30 16:46:32 -0400

    Developer portal in making - dashboarding, dependency graph - in progress

--- devportal_django_ui/ui/views.py
+++ devportal_django_ui/ui/views.py
@@ -191,8 +191,16 @@ def admin(request):
 
 
 def dashboard(request):
+    from plotly.offline import plot
+    from plotly.graph_objs import Scatter
     to, ad, au = determine(request)
-    return render(request, 'ui/dashboard.html', getbody(au, ad))
+    x_data = [0,1,2,3]
+    y_data = [x**2 for x in x_data]
+    plot_div = plot([Scatter(x=x_data, y=y_data,
+                             mode='lines', name='test',
+                             opacity=0.8, marker_color='green')],
+                    output_type='div')
+    return render(request, 'ui/dashboard.html', {'authenticated': au, 'admin': ad, 'plot_div': plot_div})
 
 
 def logout(request):
